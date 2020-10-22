46787662d51d3dbe0a9e737fcecf2d5f86530ea7
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-10-21 22:40:13 -0400

    added more to the workspace
    the rest of the workspace

--- src/main/java/com/devmaster1015/fandomcraft/FandomCraft.java
+++ src/main/java/com/devmaster1015/fandomcraft/FandomCraft.java
@@ -1,16 +1,22 @@
 package com.devmaster1015.fandomcraft;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
 
 @Mod("fandomcraftremix")
 public class FandomCraft
@@ -19,30 +25,29 @@ public class FandomCraft
 
     public FandomCraft() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
+    private void setup(final FMLCommonSetupEvent event) {
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
+
     }
 
 
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
+
     }
 
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
-
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+
         }
     }
 }
