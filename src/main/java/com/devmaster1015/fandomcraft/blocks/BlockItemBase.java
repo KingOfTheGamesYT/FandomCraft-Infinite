84fbcb5b183e887c2c8c985df0254d48d4f67719
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2022-01-13 15:36:44 -0500

    ported adamantium ore
    to 1.16.5

---
+++ src/main/java/com/devmaster1015/fandomcraft/blocks/BlockItemBase.java
@@ -0,0 +1,14 @@
+package com.devmaster1015.fandomcraft.blocks;
+
+import com.devmaster1015.fandomcraft.main.FandomCraft;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class BlockItemBase extends BlockItem {
+
+    public BlockItemBase(Block block) {
+        super(block, new Item.Properties().group(FandomCraft.tabFandomCraftBlocks));
+    }
+}
+
---
+++ src/main/java/com/devmaster1015/fandomcraft/blocks/adamantiumOre.java
@@ -0,0 +1,17 @@
+package com.devmaster1015.fandomcraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class adamantiumOre extends Block {
+
+    public adamantiumOre() {
+        super(Block.Properties.create(Material.ROCK)
+                .hardnessAndResistance(20.0F, 5.0F)
+                .harvestLevel(6)
+                .harvestTool(ToolType.PICKAXE)
+                .setRequiresTool());
+
+    }
+}
