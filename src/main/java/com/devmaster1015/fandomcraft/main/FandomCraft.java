7314f0cb3dbc8b4b4750ea25cc0325c760d4b8ef
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2022-01-13 15:39:18 -0500

    ported main class
    and worldgen creative tab

---
+++ src/main/java/com/devmaster1015/fandomcraft/main/FandomCraft.java
@@ -0,0 +1,64 @@
+package com.devmaster1015.fandomcraft.main;
+
+
+import com.devmaster1015.fandomcraft.world.FCOreGen;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.gen.feature.Feature;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DeferredWorkQueue;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("fandomcraft")
+public class FandomCraft{
+        public static final Logger LOGGER = LogManager.getLogger("FandomCraft");
+        public static final String MOD_ID = "fandomcraft";
+
+        public FandomCraft() {
+
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+            final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+            modEventBus.addListener(this::setup);
+
+            RegistryHandler.init();
+
+            MinecraftForge.EVENT_BUS.register(this);
+
+            FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Feature.class, EventPriority.LOW, FCOreGen::addConfigFeatures);
+
+            MinecraftForge.EVENT_BUS.addListener(FCOreGen::handleWorldGen);
+        }
+
+
+
+        private void setup(final FMLCommonSetupEvent event) {
+            DeferredWorkQueue.runLater(() -> {
+
+            });
+        }
+        private void doClientStuff(final FMLClientSetupEvent event) {
+        }
+
+        public static final ItemGroup tabFandomCraftBlocks = new ItemGroup("tabFandomCraftBlocks") {
+
+            @Override
+            public ItemStack createIcon() {
+                return new ItemStack(RegistryHandler.ROCK_STONE_BLOCK.get());
+            }
+        };
+    }
+
